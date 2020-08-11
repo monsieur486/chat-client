f5fe24f06c933e93bb4963c2aec324b6fc32d3c8
monsieur486 <monsieur486@gmail.com>
2020-08-11 19:50:58 +0200

    last compilation

--- templates/AppFrame.py
+++ templates/AppFrame.py
@@ -67,6 +67,9 @@ class AppFrame(wx.Frame):
 
         user01Infos = wx.BoxSizer(wx.HORIZONTAL)
 
+        self.user01Kick = wx.Button(self, wx.ID_ANY, u"Kick", wx.DefaultPosition, wx.DefaultSize, 0)
+        user01Infos.Add(self.user01Kick, 0, 0, 5)
+
         self.user01Led = wx.StaticBitmap(self, wx.ID_ANY,
                                          wx.Bitmap(u"public/icons/16x16_led_grey.png", wx.BITMAP_TYPE_ANY),
                                          wx.DefaultPosition, wx.DefaultSize, 0)
@@ -75,11 +78,14 @@ class AppFrame(wx.Frame):
         self.user01Deno = wx.StaticText(self, wx.ID_ANY, u"User01", wx.DefaultPosition, wx.DefaultSize, 0)
         self.user01Deno.Wrap(-1)
 
-        user01Infos.Add(self.user01Deno, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        user01Infos.Add(self.user01Deno, 0, wx.ALIGN_CENTER_VERTICAL, 5)
 
-        boardSide.Add(user01Infos, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
+        boardSide.Add(user01Infos, 0, wx.ALIGN_RIGHT | wx.ALL, 5)
 
         user02Infos = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.user02Kick = wx.Button(self, wx.ID_ANY, u"Kick", wx.DefaultPosition, wx.DefaultSize, 0)
+        user02Infos.Add(self.user02Kick, 0, 0, 5)
 
         self.user02Led = wx.StaticBitmap(self, wx.ID_ANY,
                                          wx.Bitmap(u"public/icons/16x16_led_grey.png", wx.BITMAP_TYPE_ANY),
@@ -89,11 +95,14 @@ class AppFrame(wx.Frame):
         self.user02Deno = wx.StaticText(self, wx.ID_ANY, u"User02", wx.DefaultPosition, wx.DefaultSize, 0)
         self.user02Deno.Wrap(-1)
 
-        user02Infos.Add(self.user02Deno, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        user02Infos.Add(self.user02Deno, 0, wx.ALIGN_CENTER_VERTICAL, 5)
 
-        boardSide.Add(user02Infos, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
+        boardSide.Add(user02Infos, 1, wx.ALIGN_RIGHT | wx.ALL, 5)
 
         user03Infos = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.user03Kick = wx.Button(self, wx.ID_ANY, u"Kick", wx.DefaultPosition, wx.DefaultSize, 0)
+        user03Infos.Add(self.user03Kick, 0, 0, 5)
 
         self.user03Led = wx.StaticBitmap(self, wx.ID_ANY,
                                          wx.Bitmap(u"public/icons/16x16_led_grey.png", wx.BITMAP_TYPE_ANY),
@@ -103,9 +112,9 @@ class AppFrame(wx.Frame):
         self.user03Deno = wx.StaticText(self, wx.ID_ANY, u"User03", wx.DefaultPosition, wx.DefaultSize, 0)
         self.user03Deno.Wrap(-1)
 
-        user03Infos.Add(self.user03Deno, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        user03Infos.Add(self.user03Deno, 0, wx.ALIGN_CENTER_VERTICAL, 5)
 
-        boardSide.Add(user03Infos, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
+        boardSide.Add(user03Infos, 1, wx.ALIGN_RIGHT | wx.ALL, 5)
 
         user04Infos = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -117,14 +126,17 @@ class AppFrame(wx.Frame):
         self.user04Deno = wx.StaticText(self, wx.ID_ANY, u"User04", wx.DefaultPosition, wx.DefaultSize, 0)
         self.user04Deno.Wrap(-1)
 
-        user04Infos.Add(self.user04Deno, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        user04Infos.Add(self.user04Deno, 0, wx.ALIGN_CENTER_VERTICAL, 5)
 
-        boardSide.Add(user04Infos, 0, wx.ALIGN_CENTER_HORIZONTAL, 5)
-
-        self.dispoBtn = wx.Button(self, wx.ID_ANY, u"Pause", wx.DefaultPosition, wx.DefaultSize, 0)
-        boardSide.Add(self.dispoBtn, 1, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
+        boardSide.Add(user04Infos, 1, wx.ALIGN_RIGHT | wx.ALL, 5)
 
         appPage.Add(boardSide, 1, wx.ALIGN_CENTER_VERTICAL, 5)
+
+        bSizer16 = wx.BoxSizer(wx.VERTICAL)
+
+        bSizer16.Add((0, 0), 0, wx.ALL, 25)
+
+        appPage.Add(bSizer16, 0, wx.EXPAND, 5)
 
         mainPage.Add(appPage, 0, wx.ALL | wx.EXPAND, 5)
 
@@ -143,7 +155,9 @@ class AppFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.onMenuQuit, id=self.m_menuItem1.GetId())
         self.userPwd.Bind(wx.EVT_TEXT_ENTER, self.cnxCnxBtn)
         self.cnxBtn.Bind(wx.EVT_BUTTON, self.cnxCnxBtn)
-        self.dispoBtn.Bind(wx.EVT_BUTTON, self.onDispoBtn)
+        self.user01Kick.Bind(wx.EVT_BUTTON, self.onUser01KickBtn)
+        self.user02Kick.Bind(wx.EVT_BUTTON, self.onUser02KickBtn)
+        self.user03Kick.Bind(wx.EVT_BUTTON, self.onUser03KickBtn)
         self.mailBox.Bind(wx.EVT_TEXT, self.onImportChange)
 
     def onImportChange(self, event):
@@ -173,5 +187,11 @@ class AppFrame(wx.Frame):
     def cnxCnxBtn(self, event):
         event.Skip()
 
-    def onDispoBtn(self, event):
+    def onUser01KickBtn(self, event):
+        event.Skip()
+
+    def onUser02KickBtn(self, event):
+        event.Skip()
+
+    def onUser03KickBtn(self, event):
         event.Skip()
