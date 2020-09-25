e6acec52f1ff42346fd776f8daf5022dde693963
monsieur486 <monsieur486@gmail.com>
2020-09-25 09:43:16 +0200

    last compilation

---
+++ web/sendkeys.js
@@ -0,0 +1,15 @@
+// send keys to websocket and echo the response
+$(document).ready(function() {
+    // create websocket
+    if (! ("WebSocket" in window)) WebSocket = MozWebSocket; // firefox
+    var socket = new WebSocket("ws://localhost:12534");
+
+    // show server response
+    socket.onmessage = function(e) {
+        $("#output").text(e.data);
+    }
+
+    // for each typed key send #entry's text to server
+    $("#entry").keyup(function (e) {
+        socket.send($("#entry").attr("value")+"\n");
+    });}
